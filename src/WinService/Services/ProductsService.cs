9445d4f53bfd1061333995a2d8df0d6e3e0a72d7
rafal.wielicki <rafal.wielicki@gmail.com>
2024-02-11 15:08:53 +0100

    Update ProductsService.cs

--- src/WinService/Services/ProductsService.cs
+++ src/WinService/Services/ProductsService.cs
@@ -20,46 +20,45 @@ namespace WinService.Services
         public DatabaseConfiguration DatabaseConfiguration { get; set; } = new DatabaseConfiguration();
         public PropertiesConfiguration PropertiesConfiguration { get; set; } = new PropertiesConfiguration();
 
+        int index;
+        
         public async Task<ProductDetailsModel?> GetProductDetails(string ean)
         {
-            try
-            {
-                using (var db = new CdnDatabaseClient(DatabaseConfiguration))
-                {
-                    db.LogError += InvokeLogError;
-
-                    var id = await db.GetProductId(ean);
-                    if (id == null)
-                        return null;
-
-                    return await db.GetProductDetails(id.Value, PropertiesConfiguration.WarehouseId);
-                }
-            }
-            catch (Exception ex)
-            {
-                InvokeLogError(ex.ToString());
-            }
-
-            return null;
-
-            //return Task.Run(() =>
-            //{                
-            //    ProductModel? model = null;                
-            //    Thread.Sleep(1000);
-
-            //    var xmodel = new ProductModel()
+            //try
+            //{
+            //    using (var db = new CdnDatabaseClient(DatabaseConfiguration))
             //    {
-            //        Code = $"TX{index}",
-            //        Name = $"TowarX {index}",
-            //        Ean = $"12345{index}",
-            //        Price = index * 0.1M,
-            //        Count = index
-            //    };
+            //        db.LogError += InvokeLogError;
 
-            //    index++;
+            //        var id = await db.GetProductId(ean);
+            //        if (id == null)
+            //            return null;
 
-            //    return xmodel ?? model;
-            //});            
+            //        return await db.GetProductDetails(id.Value, PropertiesConfiguration.WarehouseId);
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    InvokeLogError(ex.ToString());
+            //}
+
+            //return null;
+
+            Thread.Sleep(1000);
+
+            var model = new ProductDetailsModel()
+            {
+                Id = index,
+                Code = $"TX{index}",
+                Name = $"TowarX {index}",
+                Ean = $"12345{index}",
+                Price = index * 0.1M,
+                Count = index
+            };
+
+            index++;
+
+            return model;            
         }
     }
 }
